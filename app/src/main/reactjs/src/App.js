d25a8d79fd9eb1041ba377df59f8cd3028596eb5
eomjinyoung <jinyoung.eom@gmail.com>
2023-05-02 16:59:56 +0900

    .

--- app/src/main/reactjs/src/App.js
+++ app/src/main/reactjs/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          테스트1! Edit <code>src/App.js</code> and save to reload.
+          테스트222222! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
